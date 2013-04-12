e891ba3b834c0dc2c0673129a24b4e7b2fa2f37b
MilesWilford <miles.wilford@gmail.com>
2013-04-12 10:52:45 -0400

    Added packed markCurrentPage

--- js/plugins.js
+++ js/plugins.js
@@ -24,7 +24,7 @@
 // The following plugins were all made by Miles Wilford
 
 /*
- * This plugin checks a pile of anchor tags and adds the css class
+ * This plugin checks a grouping of anchor tags and adds the css class
  * "currentPage" to any anchor tags that represent the current page
  *
  * Make sure you define .currentPage appropriately in your CSS
@@ -34,10 +34,15 @@
     var pageUrl = window.location.pathname;
     return this.each(function() {
         var $this = $(this);
+
+        // If it's not an anchor, we aren't interested
         if (!$this.is('a')) {
             return false;
         }
+
+        // Get our anchor's href
         var link = this.href;
+
         // If the URL has /'s in it, we only want the last piece
         var filename = link.split('/').pop();
 
@@ -46,15 +51,19 @@
          * is an index page.  If filename represents an index page, then go ahead and add
          * filename to pageUrl so things pan out nicely.
          */
-        if ((pageUrl.substr(pageUrl.length - 1, 1) == '/'
-        || pageUrl.indexOf('/?') != -1)
-        && filename.indexOf('index') != -1) {
+        if ((pageUrl.substr(pageUrl.length - 1, 1) == '/' || pageUrl.indexOf('/?') != -1)
+            && filename.indexOf('index') != -1) {
                 pageUrl += filename;
         }
 
+        // Now check if the current pageUrl contains the filename from the link's href
         if (pageUrl.indexOf(filename) != -1) {
             $this.addClass('currentPage');
         }
     });
   };
 })( jQuery );
+
+// Same as above, but packed with Packer 3.0
+(function($){$.fn.markCurrentPage=function(){var d=window.location.pathname;return this.each(function(){var a=$(this);if(!a.is('a')){return false}var b=this.href;var c=b.split('/').pop();if((d.substr(d.length-1,1)=='/'||d.indexOf('/?')!=-1)&&c.indexOf('index')!=-1){d+=c}if(d.indexOf(c)!=-1){a.addClass('currentPage')}})}})(jQuery);
+
